40f0d9403b28483bef8df65b79fcefa9692aedfd
Jimmy McBroom <james.mcbroom@gmail.com>
2019-02-13 12:25:36 -0500

    tuesday work committed

--- dags/dah.py
+++ dags/dah.py
@@ -1,11 +1,9 @@
 from airflow import DAG
 import datetime as dt
-import os, re
+import os, re, yaml
+from common import sources
 
 from airflow.models import Variable
-
-from airflow.hooks.mssql_hook import MsSqlHook
-from airflow.hooks.postgres_hook import PostgresHook
 
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.bash_operator import BashOperator
@@ -18,124 +16,41 @@ default_args = {
     'retries': 0
 }
 
-# source tables we'll bring into postgres
-tables = [
-    {
-        "mssql": "SWEETSpower.tblDAHPayments",
-        "where": "",
-        "psql": "dah.payments"
-    },
-    {
-        "mssql": "SWEETSpower.tblZTickets",
-        "where": """where (('2000-01-01' < "IssueDate" and "IssueDate" < '2025-01-01') or "IssueDate" is null)""",
-        "psql": "dah.ztickets"
-    },
-    {
-        "mssql": "SWEETSpower.tblDispAdjourn",
-        "where": "",
-        "psql": "dah.dispadjourn"
-    },
-    {
-        "mssql": "SWEETSpower.tblDAHCityFines",
-        "where": "",
-        "psql": "dah.cityfines"
-    },
-    {
-        "mssql": "SWEETSpower.tblDAHBlightTicketServiceCostTransactions",
-        "where": "",
-        "psql": "dah.blight_ticket_svc_cost"
-    },
-    {
-        "mssql": "SWEETSpower.tblReSchedule",
-        "where": """where (('2000-01-01' < "ProcessDate" and "ProcessDate" < '2025-01-01') or "ProcessDate" is null)""",
-        "psql": "dah.reschedule"
-    },
-    {
-        "mssql": "SWEETSpower.tblCourtTime",
-        "where": "",
-        "psql": "dah.courttime"
-    },
-    {
-        "mssql": "SWEETSpower.tblAgency",
-        "where": "",
-        "psql": "dah.agency"
-    },
-    {
-        "mssql": "SWEETSpower.tblDAHOrdinance",
-        "where": "",
-        "psql": "dah.ordinance"
-    },
-    {
-        "mssql": "SWEETSpower.tblStreets",
-        "where": "",
-        "psql": "dah.streets"
-    },
-    {
-        "mssql": "SWEETSpower.tblDAHViolatorAddress",
-        "where": "",
-        "psql": "dah.violator_address"
-    },
-    {
-        "mssql": "SWEETSpower.tblState",
-        "where": "",
-        "psql": "dah.state"
-    },
-    {
-        "mssql": "SWEETSpower.tblDAHViolatorInfo",
-        "where": "",
-        "psql": "dah.violator_info"
-    },
-    {
-        "mssql": "SWEETSpower.tblSecurity",
-        "where": "",
-        "psql": "dah.security"
-    },
-    {
-        "mssql": "SWEETSpower.tblDAHCountry",
-        "where": "",
-        "psql": "dah.country"
-    },
-    {
-        "mssql": "SWEETSpower.tblDAHDispType",
-        "where": "",
-        "psql": "dah.disp_type"
-    }
-]
-
-# Get records from MS SQLServer and insert into Postgres
-def ms_to_pg(table):
-    mshook = MsSqlHook('dah_database')
-    records = mshook.get_records("select * from {} {}".format(table['mssql'], table['where']))
-
-    pghook = PostgresHook('etl_postgres')
-    pghook.insert_rows("{}".format(table['psql']), records)
-
 with DAG('dah', 
     default_args=default_args,
     schedule_interval="0 1 * * *") as dag:
 
-    opr_dummy = BashOperator(
-        task_id='dummy',
-        bash_command="echo 'fake'"
+    opr_wait = BashOperator(
+        task_id='wait',
+        bash_command="echo 'wait for all extracts to finish'"
     )
 
-    # Extract
-    for t in tables:
+    opr_transform = BashOperator(
+        task_id='transform',
+        bash_command='psql -d etl -f /home/gisteam/airflow/dah/blight_violations.sql'
+    )
 
-        opr_truncate_table = PostgresOperator(
-            task_id=f"truncate_{t['psql']}",
-            sql=f"truncate table {t['psql']} cascade",
+    sources_to_extract = yaml.load(open(f"{dag.dag_id}/_sources.yml"))
+
+    for t, s in sources_to_extract.items():
+
+        s['name'] = t
+        s['dag'] = dag.dag_id
+
+        opr_truncate = PostgresOperator(
+            task_id=f"truncate_{t}",
+            sql=f"truncate table {dag.dag_id}.{t} cascade",
             postgres_conn_id='etl_postgres'
         )
 
-        opr_extract_table = PythonOperator(
-            task_id=f"get_{t['mssql']}",
-            python_callable=ms_to_pg,
-            provide_context=False,
-            op_kwargs = {
-                "table": t
-            }
+        opr_extract = PythonOperator(
+            task_id=f"extract_{t}",
+            python_callable=sources.extract_source,
+            provide_context=True,
+            op_kwargs=s
         )
 
-        opr_dummy.set_downstream(opr_truncate_table)
-        opr_truncate_table.set_downstream(opr_extract_table)
+        opr_truncate.set_downstream(opr_extract)
+        opr_extract.set_downstream(opr_wait)
+
+opr_wait >> opr_transform
